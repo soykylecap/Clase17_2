036329b90ddd9421674ed982657d579ffbc0af4b
soykylecap <soykyle@gmail.com>
2024-07-24 07:05:15 -0300

    NavBar y Admin de Django

--- AppKyle/admin.py
+++ AppKyle/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from AppKyle.models import Curso, Estudiante, Profesor
 
 # Register your models here.
+admin.site.register(Curso)
+admin.site.register(Estudiante)
+admin.site.register(Profesor)
